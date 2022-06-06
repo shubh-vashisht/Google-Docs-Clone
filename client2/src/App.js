b677457d1dc58ecc863b17ed643e94edf81331c0
Shubh <shubhv2@gmail.com>
2022-06-06 02:36:57 -0400

    second commit

---
+++ client2/src/App.js
@@ -0,0 +1,24 @@
+import TextEditor from "./TextEditor";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          exact
+          element={<Navigate to={`/documents/${uuidv4()}`} replace />}
+        />
+        <Route path="/documents/:id" exact element={<TextEditor />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
